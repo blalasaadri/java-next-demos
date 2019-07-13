d2f74d9a6008c7d32b9b9b3327d4387ab1c40eda
Alasdair Collinson <blalasaadri@googlemail.com>
2019-07-13 14:16:20 +0000

    Created Java 10 project

---
+++ java-10-examples/src/test/java/com/github/blalasaadri/javaNextDemos/AppTest.java
@@ -0,0 +1,12 @@
+package com.github.blalasaadri.javaNextDemos;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
