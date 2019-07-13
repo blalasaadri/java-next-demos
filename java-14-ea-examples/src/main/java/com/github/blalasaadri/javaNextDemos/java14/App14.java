1946731d41b9e8b5075248a80eb23b00b83f64f9
Alasdair Collinson <blalasaadri@googlemail.com>
2019-07-13 15:33:52 +0000

    Made the Java 14 JAR executable

--- java-14-ea-examples/src/main/java/com/github/blalasaadri/javaNextDemos/java14/App14.java
+++ java-14-ea-examples/src/main/java/com/github/blalasaadri/javaNextDemos/java14/App14.java
@@ -1,6 +1,6 @@
 package com.github.blalasaadri.javaNextDemos.java14;
 
-public class App {
+public class App14 {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
