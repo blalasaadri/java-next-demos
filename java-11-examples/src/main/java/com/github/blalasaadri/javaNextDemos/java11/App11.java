bf8d2db4a6d33a2b56b994bc49a834f9bd9288ef
Alasdair Collinson <blalasaadri@googlemail.com>
2019-07-13 15:29:18 +0000

    Made the Java 11 JAR executable

--- java-11-examples/src/main/java/com/github/blalasaadri/javaNextDemos/java11/App11.java
+++ java-11-examples/src/main/java/com/github/blalasaadri/javaNextDemos/java11/App11.java
@@ -1,6 +1,6 @@
 package com.github.blalasaadri.javaNextDemos.java11;
 
-public class App {
+public class App11 {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
