d284b35947a7cd7e0e2f87e1f8ee1ec840267492
Alasdair Collinson <blalasaadri@googlemail.com>
2019-07-13 15:31:59 +0000

    Made the Java 13 JAR executable

--- java-13-ea-examples/src/main/java/com/github/blalasaadri/javaNextDemos/java13/App13.java
+++ java-13-ea-examples/src/main/java/com/github/blalasaadri/javaNextDemos/java13/App13.java
@@ -1,6 +1,6 @@
 package com.github.blalasaadri.javaNextDemos.java13;
 
-public class App {
+public class App13 {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
--- java-13-ea-examples/src/test/java/com/github/blalasaadri/javaNextDemos/java13/App13Test.java
+++ java-13-ea-examples/src/test/java/com/github/blalasaadri/javaNextDemos/java13/App13Test.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AppTest {
+public class App13Test {
 
     @Test
     public void shouldAnswerWithTrue() {
