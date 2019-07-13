f310e53c4e6f5a7fb5bfa4cc8e61a9783d923924
Alasdair Collinson <blalasaadri@googlemail.com>
2019-07-13 15:30:55 +0000

    Made the Java 12 JAR executable

--- java-12-examples/src/main/java/com/github/blalasaadri/javaNextDemos/java12/App12.java
+++ java-12-examples/src/main/java/com/github/blalasaadri/javaNextDemos/java12/App12.java
@@ -1,6 +1,6 @@
 package com.github.blalasaadri.javaNextDemos.java12;
 
-public class App {
+public class App12 {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
