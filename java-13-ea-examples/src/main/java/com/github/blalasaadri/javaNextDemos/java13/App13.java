3f5591847db43d99c05f871f3e3960db9af00b68
Alasdair Collinson <Alasdair.Collinson@senacor.com>
2019-07-24 01:57:42 +0200

    Added example for JDK 13 switch expressions

--- java-13-ea-examples/src/main/java/com/github/blalasaadri/javaNextDemos/java13/App13.java
+++ java-13-ea-examples/src/main/java/com/github/blalasaadri/javaNextDemos/java13/App13.java
@@ -2,6 +2,30 @@ package com.github.blalasaadri.javaNextDemos.java13;
 
 public class App13 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(greet(args[0], args[1]));
+    }
+
+    static String greet(String greeting, String name) {
+//        String greetingFormat = switch (greeting) {
+//            case "Hi" -> "Hi %s! How are you doing?";
+//            case "Hey" -> "Hey there, %s.";
+//            case "Hay", "Hj" -> "Howdy %s, you should check your spelling. ;-)";
+//            default -> {
+//                System.err.println("I don't know that greeting. Falling back to default.");
+//                yield "Hello, %s!";
+//            }
+//        };
+
+        // Alternative syntax:
+        String greetingFormat = switch (greeting) {
+            case "Hi": yield "Hi %s! How are you doing?";
+            case "Hey": yield "Hey there, %s.";
+            case "Hay", "Hj": yield "Howdy %s, you should check your spelling. ;-)";
+            default:
+                System.err.println("I don't know that greeting. Falling back to default.");
+                yield "Hello, %s!";
+        };
+
+        return String.format(greetingFormat, name);
     }
 }
--- java-13-ea-examples/src/test/java/com/github/blalasaadri/javaNextDemos/java13/App13Test.java
+++ java-13-ea-examples/src/test/java/com/github/blalasaadri/javaNextDemos/java13/App13Test.java
@@ -1,14 +1,44 @@
 package com.github.blalasaadri.javaNextDemos.java13;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class App13Test {
 
     @Test
-    void shouldAnswerWithTrue() {
-        assertThat(true).isTrue();
+    void forHi_shouldGreetWithQuestion() {
+        String greeting = App13.greet("Hi", "World");
 
+        assertThat(greeting)
+                .isEqualTo("Hi World! How are you doing?");
+    }
+
+    @Test
+    void forHey_shouldGreetWithHeyThere() {
+        String greeting = App13.greet("Hey", "World");
+
+        assertThat(greeting)
+                .isEqualTo("Hey there, World.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "Hay", "Hj" })
+    void forMisspellings_shouldGreetWithCorrection(String prompt) {
+        String greeting = App13.greet(prompt, "World");
+
+        assertThat(greeting)
+                .endsWith("you should check your spelling. ;-)");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "¡Hola!", "Ciao" })
+    void forUnknownGreetings_shouldGreetWithHello(String prompt) {
+        String greeting = App13.greet(prompt, "World");
+
+        assertThat(greeting)
+                .isEqualTo("Hello, World!");
     }
 }
